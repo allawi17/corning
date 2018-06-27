0743fc4963682198b9a15fbad1d9765994617aac
Alaa Abed <alaa@appnovation.com>
2018-06-27 16:05:11 -0400

    Add client id/secret.

--- js/services.js
+++ js/services.js
@@ -2,10 +2,18 @@ angular.module('employeeApp.services', []).factory('Employee', function($resourc
     return $resource('http://corning-identity-service-sandbox-v1.cloudhub.io/api/employees/:id', {id: '@ID'}, {
         query: {
             method: 'GET',
-            isArray: true
+            isArray: true,
+            headers: { 
+               'client_id': 'aac474269c204c25a6ffc7a73cbdaefc',
+               'client_secret': 'e91b356f0ed54Eb48E272ce91916536e'
+            }
         },
         get: {
-            method: 'GET'
+            method: 'GET',
+            headers: { 
+               'client_id': 'aac474269c204c25a6ffc7a73cbdaefc',
+               'client_secret': 'e91b356f0ed54Eb48E272ce91916536e'
+            }
         }
     });
 }).service('popupService', function($window) {
